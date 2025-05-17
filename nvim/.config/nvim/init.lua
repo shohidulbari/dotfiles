9a46294b4b0766764bb53bf7fe0a01a2166dc802
Shohidul Bari <sbr@Shohiduls-Air.lan>
2025-05-18 01:47:11 +0600

    use lazyvim and update zshrc

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,2 @@
-require("shohidulbari.core")
-require("shohidulbari.lazy")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- nvim/.config/nvim/lua/shohidulbari/core/init.lua
+++
@@ -1,2 +0,0 @@
-require("shohidulbari.core.options")
-require("shohidulbari.core.keymaps")
